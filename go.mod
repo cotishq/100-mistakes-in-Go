014fb4fea907e44ecf0297a1b6f7ff0c4d0be8cb
cotishq <tanishqp101204@gmail.com>
2026-01-15 23:57:22 +0530

    01

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cotishq/100-mistakes-in-Go
+
+go 1.25.5
---
+++ mistakes/01-shadowing/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	tracing := true
+
+	var client *http.Client // outer client
+
+	if tracing {
+		client, err := createClientWithTracing() //  shadowing happens here
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("inside if client:", client)
+	} else {
+		client, err := createDefaultClient() //  shadowing happens here
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("inside else client:", client)
+	}
+
+	// This prints nil because outer client never got assigned
+	log.Println("outside client:", client)
+}
+
+func createClientWithTracing() (*http.Client, error) {
+	return &http.Client{}, nil
+}
+
+func createDefaultClient() (*http.Client, error) {
+	return &http.Client{}, nil
+}
