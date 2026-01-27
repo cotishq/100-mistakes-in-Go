f7b84e889eace7fa2419253eac66e3b5fe129b5d
cotishq <tanishqp101204@gmail.com>
2026-01-27 23:48:38 +0530

    09-generics with examples

--- mistakes/09-generics/main.go
+++ mistakes/09-generics/main.go
@@ -1,19 +1,49 @@
-func getKeys(m any) ([]any , error) {
-	switch t := m.(type) {
-	default:
-		return nil , fmt.Errorf("unknown type: %T", t)
-	case map[string]int:
-		var keys []any
-		for k := range t {
-			keys = append(keys, k)
-		}
+package main
 
-		return keys, nil
-	case map[int]string:
-		var keys []any
-for k := range t {
-keys = append(keys, k)
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Before generics")
+	m1 := map[string]int{"one": 1, "two": 2}
+	fmt.Println(getStringIntkeys(m1))
+
+	fmt.Println("After generics")
+	m2 := map[int]string{1: "one", 2: "two"}
+	fmt.Println(getKeys(m1))
+	fmt.Println(getKeys(m2))
 }
-		
+
+
+
+func getStringIntkeys(m map[string]int) []string {
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
 	}
+	return keys
+
 }
+
+//if we want map[int]string keys, we need another function
+func getIntStringKeys(m map[int]string) []int {
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+
+}
+
+// Correct use of generics
+// K must be comparable because map keys must be comparable
+func getKeys[K comparable, V any](m map[K]V) []K {
+	var keys []K
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+
